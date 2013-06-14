e73f7dff298dde48a4427dd73a82d85ac73f0cd0
Gregor MacLennan <gmaclennan@digital-democracy.org>
2013-06-14 12:06:18 -0700

    Major refactor and cleanup of map code

--- mapstory.js
+++ mapstory.js
@@ -31,9 +31,9 @@
   //-- End of customizable variables --//
   //-- ***************************** --//
   
-  var communityLayer,
+  var satLayer,
       labelLayer,
-      satLayer,
+      communityLayer,
       markerLayer,
       markerGeoJson,
       closeTooltip,
@@ -110,133 +110,26 @@
   // _onOverlayLoad adds geojson returned from the JSONP call to the map
   // and caches the bounds of each nationality in bounds[]
   var _onOverlayLoad = function(geojson) {
-    communityLayer = d3layer().data(geojson);
-    map.addLayer(communityLayer);
+    communityLayer.data(geojson);
   }
 
   // _onMarkerLoad processes the Google JSON returned from the spreadsheet
   // and adds it to the marker layer.
   var _onMarkerLoad = function(geojson) {
     markerLayer.features(geojson.features);
-    map.addLayer(markerLayer);
+    var interaction = mapbox.markers.interaction(markerLayer);
+    // Set a custom formatter for tooltips
+    // Provide a function that returns html to be used in tooltip
+    interaction.formatter(function(feature) {
+        var o = feature.properties.name;
+        return o;
+    });
   }
 
   var _ease = function () {
-    // IE 8
-    if (window.pageYOffset === undefined) {
-      var y = document.documentElement.scrollTop;
-      var h = document.documentElement.clientHeight;
-    } else {
-      var y = window.pageYOffset;
-      var h = window.innerHeight;
-    }
+    y = $(window).scrollTop();
     t = y%1000/1000;
     MapStory.ease.t(t);
-  }
-
-  // Check window position and set active section.
-  // *TODO* check if jQuery can replace IE8 specific code.
-  var _checkActiveSection = function () {
-    // IE 8
-    if (window.pageYOffset === undefined) {
-      var y = document.documentElement.scrollTop;
-      var h = document.documentElement.clientHeight;
-    } else {
-      var y = window.pageYOffset;
-      var h = window.innerHeight;
-    }
-
-    // If scrolled to the very top of the page set the first section active.
-    if (y === 0) return setActiveSection(0, true);
-
-    // Otherwise, conditionally determine the extent to which page must be
-    // scrolled for each section. The first section that matches the current
-    // scroll position wins and exits the loop early.
-    var memo = 0;
-    var buffer = (h * 0.3333);
-    var active = _(sections).any(function(el, index) {
-      memo += el.offsetHeight;
-      return y < (memo-buffer) ? setActiveSection(index, true) : false;
-    });
-
-    // If no section was set active the user has scrolled past the last section.
-    // Set the last section active.
-    if (!active) setActiveSection(sections.length - 1, true);
-  }
-
-
-//  var map = L.mapbox.map('map', 'gmaclennan.map-y7pgvo15', mapOptions).fitBounds(ECUADOR_BOUNDS.padXY(0.33, 0));
-
-//  var bingLayer = new L.BingLayer(BING_API_KEY).addTo(map).bringToBack();
-
-
-
-
-  function mousemoveArea(e) {
-    var layer = e.target,
-        community = _sanitize(layer.feature.properties.nombre),
-        areaIsZoomed = (community == activeArticle);
-
-    popup.setLatLng(e.latlng);
-    popup.setContent('<h2>' + layer.feature.properties.nombre + '</h2><p>' +
-        layer.feature.properties.nacion + ' nationality<br>' +
-        'XXX Clearwater systems installed<br>' +
-        'Click to explore this community</p>');
-
-    if (!popup._map && !areaIsZoomed) popup.openOn(map);
-    window.clearTimeout(closeTooltip);
-
-    // highlight feature
-    layer.setStyle(areaHoverStyle);
-
-    if (!L.Browser.ie && !L.Browser.opera) {
-        layer.bringToFront();
-    }
-  }
-
-  function mouseoverMarker(e) {
-    var layer = e.target,
-        community = _sanitize(layer.feature.properties.nombre),
-        areaIsZoomed = (community == activeArticle);
-
-    popup.setLatLng(e.latlng);
-    popup.setContent('<h2>' + layer.feature.properties.name + '</h2>' +
-        '<img src="' + layer.feature.properties.photo + '"><br>' +
-        'Location: ' + layer.feature.properties.location + '</p>');
-
-    if (!popup._map) popup.openOn(map);
-    window.clearTimeout(closeTooltip);
-
-    if (!L.Browser.ie && !L.Browser.opera) {
-        layer.bringToFront();
-    }
-  }
-
-
-  function mouseout(e) {
-    communityLayer.resetStyle(e.target);
-    closeTooltip = window.setTimeout(function() {
-        map.closePopup();
-    }, 100);
-  }
-
-  function zoomToFeature(e) {
-    var community = _sanitize(e.target.feature.properties.nombre);
-    // If we are already looking at this community, do nothing.
-    if (community == activeArticle) return true;
-    e.target.off('mousemove', mousemoveArea);
-    mouseout(e);
-    activeArticle = community;
-  
-    _(articles).each(function(s) { s.className = s.className.replace(' active', '') });
-    articles[1].className += ' active';
-    sections = articles[1].getElementsByTagName('section');
-    setActiveSection(0);
-    window.scrollTo(0,0);
-    communityLayer.resetStyle(e.target);
-    oms.clearMarkers();
-    markerLayer.clearLayers().addData(installationGeoJson);
-    map.fitBounds(e.target.getBounds());
   }
 
   // Helper to _sanitize a string, replacing spaces with "-" and lowercasing
@@ -256,7 +149,7 @@
           g = svg.append("g");
           
       var click = function (d) {
-          window.location = "http://amazonwatch.org/work/" + d.properties.name;
+          window.location = "#" + d.properties.name;
       }
 
       f.parent = div.node();
@@ -274,7 +167,7 @@
             .style("margin-top", "0px") && (first = false);
 
         path = d3.geo.path().projection(f.project);
-        feature.attr("d", path);
+        if (typeof feature !== 'undefined') feature.attr("d", path);
       };
 
       f.data = function(x) {
