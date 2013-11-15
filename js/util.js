59d53336998a90747d2440fca48314c3b61a5a32
Gregor MacLennan <gmaclennan@digital-democracy.org>
2013-11-14 19:14:39 -0800

    Add comment

--- js/util.js
+++ js/util.js
@@ -8,6 +8,7 @@ cwm.util = {
     return function () { return (document.documentElement || document.body.parentNode || document.body).scrollTop; };
   })(),
   
+  // Polyfill for window.innerHeight on IE8 and below.
   windowHeight: (function () {
     if (window.innerHeight !== undefined) return function () { return window.innerHeight };
     if (document.documentElement && document.documentElement.clientHeight) return function () { return document.documentElement.clientHeight; };
