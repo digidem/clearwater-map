432ab550c1d35a25abd8781d70ad8b61369c9fbd
Gregor MacLennan <gmaclennan@digital-democracy.org>
2013-11-27 10:12:06 -0800

    Improvements to flight handler

--- js/handlers/flight_handler.js
+++ js/handlers/flight_handler.js
@@ -16,6 +16,8 @@ cwm.handlers.FlightHandler = function () {
       override,
       map,
       easings,
+      ease,
+      paused,
       locations,
       lastScroll,
       enabled = false;
@@ -25,6 +27,7 @@ cwm.handlers.FlightHandler = function () {
   // Expects a mapbox v.0.6.x map object (ModestMaps)
   flightHandler.map = function (m) {
     map = m;
+    ease = mapbox.ease().map(map);
     return flightHandler;
   };
 
@@ -52,6 +55,7 @@ cwm.handlers.FlightHandler = function () {
   // Moves the map to the location corresponding to the current scroll position.
   // Returns false if there is no easing for this location.
   flightHandler.easeTo = function (scrollTop) {
+    if (paused) return flightHandler;
     scrollTop = Math.max(scrollTop, 0);
 
     if (!!override) {
@@ -77,21 +81,35 @@ cwm.handlers.FlightHandler = function () {
   // points far apart on the page without moving through the intermediary steps 
   flightHandler.setOverride = function (from,start,top,bottom) {
     from = from || map.coordinate.copy(),
-    start = start || window.pageYOffset,
+    start = start || cwm.scrollHandler.currentScroll(),
     top = Math.max(top || start - 200, 0);
     bottom = bottom || start + 200;
 
+    flightHandler.clearOverride();
+    
     var ease1, ease2, topCoord, bottomCoord;
 
-    override = {top: top, bottom: bottom};
+    override = {top: top, bottom: bottom };
     topCoord = easings[Math.floor(top)] || _.last(easings);
     bottomCoord = easings[Math.floor(bottom)] || _.last(easings);
-    ease1 = map.ease.from(topCoord).to(from).setOptimalPath();
-    ease2 = map.ease.from(from).to(bottomCoord).setOptimalPath();
-    override.easings = ease1.future(start - top).concat(ease2.future(bottom-start));
-    override.time = ease1.getOptimalTime() + ease2.getOptimalTime();
+    ease1 = ease.from(topCoord).to(from).setOptimalPath();
+    override.easings = ease1.future(start - top);
+    override.time = ease1.getOptimalTime();
+    ease2 = ease.from(from).to(bottomCoord).setOptimalPath();
+    override.easings = override.easings.concat(ease2.future(bottom - start));
+    override.time += ease2.getOptimalTime();
     return flightHandler;
   };
+  
+  flightHandler.pause = function () {
+    paused = true;
+    return flightHandler;
+  };
+  
+  flightHandler.resume = function () {
+    paused = false;
+    return flightHandler;
+  }
   
   flightHandler.clearOverride = function () {
     override = undefined;
@@ -130,7 +148,7 @@ cwm.handlers.FlightHandler = function () {
     _.forEach(locations, function (v) {
       coord = map.centerFromBounds(v.bounds);
       if (!!prevCoord) {
-        easing = map.ease.from(prevCoord).to(coord)
+        easing = ease.from(prevCoord).to(coord)
                   .easing('linear').setOptimalPath();
         // for some reason the first easing is funky, so we drop it...
         coords = _.tail(easing.future(v.scrollPoint - prevScrollPoint + 1));
