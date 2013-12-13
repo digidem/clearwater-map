ba1261ea86a347ccc617725a5c2b933546887c9f
Gregor MacLennan <gmaclennan@digital-democracy.org>
2013-12-13 10:40:35 -0800

    Revert "Change collection to classical object"
    This reverts commit 25628ec43374921b4a58c8684ce463865ceb3e2b.

--- js/src/core/collection.js
+++ js/src/core/collection.js
@@ -3,76 +3,59 @@
  * @param {text} id Identifier for this collection
  */
 cwm.Collection = function(id) {
-	var event = d3.dispatch("changed");
+	var event = d3.dispatch("changed"),
+		byId = {},
+		byParent = {},
+		places,
+		parents = [],
+		bounds;
 
-	if (!(this instanceof cwm.Collection))
-		return new cwm.Collection();
-	this.id = id;
-	this._byId = {};
-
-	return d3.rebind(this, event, 'on');
-};
-
-cwm.Collection.prototype = {
-	defaults: {
+	var defaults = {
 		// id is either a string or a function which returns the id
 		id: function(d) { return d.properties.id; },
 		storyKey: "story",
 		titleKey: "title",
 		parent: null
-	},
+	};
 
-	add: function(geojson, options) {
-		var allByParent = this._allByParent,
-			places = this.places,
-			byId = this._byId,
-			allById = this._allById;
+	var collection = {
+		add: function(geojson, options) {
+			_.defaults(options, defaults);
+			// Faster to create a fixed size array http://jsperf.com/array-push-vs-array-length23333/8
+			places = new Array(geojson.features.length);
+			bounds = d3.geo.bounds(geojson);
 
-		_.defaults(options, this.defaults);
-		// Faster to create a fixed size array http://jsperf.com/array-push-vs-array-length23333/8
-		this.places = new Array(geojson.features.length);
-		this.bounds = d3.geo.bounds(geojson);
+			// Create a new place for each feature in the geoJSON
+			geojson.features.forEach(function(feature, i) {
+				var place = cwm.Place(feature, options).on("changed", event.changed);
+				// Build an array of unique parents referenced from incoming data
+				if (!(place.parent in byParent)) byParent[place.parent] = [];
+				byParent[place.parent].push(place);
+				byId[place.id] = place;
+				places[i] = place;
+			});
+			return collection;
+		},
 
-		// Create a new place for each feature in the geoJSON
-		geojson.features.forEach(function(feature, i) {
-			// When a place changes, trigger change event on collection
-			var place = cwm.Place(feature, options).on("changed", event.changed);
-			// If the place's parent is not indexed, index it
-			if (!(place.parent in allByParent)) allByParent[place.parent] = [];
-			// If the place's parent is already cached, add this place as a child
-			if (place.parent in allById) allById[place.parent].children.push(place);
-			// If this place is a parent to a place already cached, add that place as a child.
-			if (place.id in allByParent) place.children.push(allByParent[place.id]);
-			// Index a reference to this place by parent
-			allByParent[place.parent].push(place);
-			// Index a reference to this place by id, both in the instance and the prototype.
-			allById[place.id] = byId[place.id] = place;
-			// Add the place to the instance places array
-			places[i] = place;
-		});
-		return this;
-	},
+		get: function(id) {
+			return byId[id];
+		},
 
-	get: function(id) {
-		return (id) ? this._byId[id] : this.places;
-	},
+		getByParent: function(parentId) {
+			return byParent[parentId];
+		},
 
-	getallByParent: function(parentId) {
-		return allByParent[parentId];
-	},
+		asGeoJSON: function() {
+			return {
+				type: "FeatureCollection",
+				features: places.map(function(place) {
+					return place.asGeoJSON();
+				})
+			};
+		},
+	};
 
-	asGeoJSON: function() {
-		return {
-			type: "FeatureCollection",
-			features: places.map(function(place) {
-				return place.asGeoJSON();
-			})
-		};
-	},
-
-	_allByParent: {},
-
-	_allById: {}
+	return d3.rebind(collection, event, 'on');
 };
 
 // // Insert new places with parent = id after their parent with that id
--- js/src/core/place.js
+++ js/src/core/place.js
@@ -18,7 +18,6 @@ cwm.Place.prototype = {
 		this.centroid = feature.geometry && d3.geo.centroid(feature);
 		this.id = options.id(feature);
 		this.parent = options.parent && d3.functor(options.parent)(feature);
-		this.children = [];
 		return this;
 	},
 
