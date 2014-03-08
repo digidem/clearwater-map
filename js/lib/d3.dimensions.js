e4a4ec41c14fd99bc9c323eefe83bf1e4c5c603e
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-03-08 11:14:29 -0500

    d3.dimensions: Return null dimensions for empty selections

--- js/lib/d3.dimensions.js
+++ js/lib/d3.dimensions.js
@@ -1,8 +1,8 @@
 d3.selection.prototype.dimensions = function (dimensions) {
     if (!arguments.length) {
         var node = this.node();
-        return [node.offsetWidth,
+        return this.node() && [node.offsetWidth,
                 node.offsetHeight];
     }
-    return this.attr({width: dimensions[0], height: dimensions[1]});
+    return this.node() && this.attr({width: dimensions[0], height: dimensions[1]});
 };
