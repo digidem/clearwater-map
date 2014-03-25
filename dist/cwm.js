6782d09356ed5cb021036467f1c9e4b92936806f
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-03-25 09:01:02 -0500

    Push development to master v1.0

--- dist/cwm.js
+++ dist/cwm.js
@@ -19420,6 +19420,16 @@ cwm.views.Overlay = function(container) {
     var overlayDiv = container.select("#overlay");
     var shown = {};
     var count = 0;
+    var distance = 0;
+    var lastMouse;
+    var hiding;
+
+    container.on("mousemove", function() {
+        var coord = d3.mouse(container.node());
+        if (lastMouse) distance += cwm.util.distance(coord, lastMouse);
+        if (distance > 400) hide();
+        lastMouse = coord;
+    });
 
     shown["overlay-installations"] = true;
 
@@ -19428,17 +19438,22 @@ cwm.views.Overlay = function(container) {
     });
 
     function hide() {
+        if (hiding) return;
+        hiding = true;
         overlayDiv.selectAll(".overlay")
                 .transition()
+                .duration(500)
                 .style("opacity", 0)
                 .each("end", function() {
                     d3.select(this).style("display", "none");
+                    container.selectAll("svg, button.nav").style("z-index", 0);
                 });
-            container.selectAll("svg, button.nav").style("z-index", 0);
+        window.setTimeout(function() { hiding = false; }, 550);
     }
 
     return {
         show: function(d) {
+            distance = 0;
             var id = cwm.util.sanitize(d.collection.id()).replace(/^id-/, "overlay-");
             if (!shown[id]) {
                 overlayDiv.selectAll("#" + id + ".overlay")
